04fb3066cf0dc36f8aeb60e42ae12dc77d3d2fea
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-12 12:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function getFirstSelector(selector){
   let item = document.querySelector(selector);
   return item;
 }
+function nestedTarget(){
+  let item = document.getElementsByClassName('nested').getElementById('target');
+  return item;
+}
